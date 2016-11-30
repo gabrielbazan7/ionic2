08c21f0932d861db2b2538a587dae393dadeddf3
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-11-30 16:39:12 -0300

    first commit ionic and angular 2 proyect

---
+++ src/pages/about/about.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+import { AlertController } from 'ionic-angular';
+
+import { ActionSheetController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController) {
+
+  }
+
+  doAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+}
